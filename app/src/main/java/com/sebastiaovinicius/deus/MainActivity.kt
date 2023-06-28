db0789c91676878dc82041efdcc4a9d36661589c
Sebastiao Vinícius Araújo de Moura <sebastianbavini@hotmail.com>
2023-06-28 17:06:51 -0300

    Adicionar ouvintes nos botoes

--- app/src/main/java/com/sebastiaovinicius/deus/MainActivity.kt
+++ app/src/main/java/com/sebastiaovinicius/deus/MainActivity.kt
@@ -2,10 +2,20 @@ package com.sebastiaovinicius.deus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.sebastiaovinicius.deus.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.sonoButton.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+
     }
 }
